d6b49573b671d5679d1be86672964a6e8705663b
ClarkChan1 <clark2chan@gmail.com>
2021-07-01 11:15:46 -0700

    got lamp light up to work so that it actuall adds a div with the light class so it doesn't block the speakers from being hovered over

---
+++ scripts/logic.js
@@ -0,0 +1,48 @@
+function lightUpLamp(lamp) {
+  let light = document.createElement('div');
+  light.classList.add('light');
+  lamp.appendChild(light);
+
+}
+
+function removeLightLamp(lamp){
+  lamp.removeChild(lamp.querySelector('.light'));
+}
+
+// function to add the light only when lamp is hovered over so it doesn't cover the speakers and you can hover over the speakers as well
+function addListenerLamp() {
+  let lamp = document.querySelector('.lamp');
+  let lampHead = document.querySelector('.lamp .head');
+  console.log(lampHead);
+  // for when user mouses over
+  lampHead.addEventListener('mouseover', function(){
+    lightUpLamp(lamp);
+  });
+  // remove the light div when user stops hovering
+  lampHead.addEventListener('mouseout', function(){
+    removeLightLamp(lamp);
+  });
+}
+
+function playMusic(speaker) {
+  console.log("in function playMusic");
+}
+
+function lightUpSpeaker(speaker) {
+  console.log("in function lightUpSpeaker");
+
+}
+
+// function to make speakers light up and play music on mouseover
+function addListenerSpeakers() {
+  let speakers = document.querySelectorAll('.speaker');
+  console.log(speakers);
+  for (let a = 0; a < speakers.length; a++) {
+    speakers[a].addEventListener('mouseover', playMusic);
+    speakers[a].addEventListener('mouseover', lightUpSpeaker);
+  }
+}
+
+// calling all the logic
+addListenerLamp();
+addListenerSpeakers();
