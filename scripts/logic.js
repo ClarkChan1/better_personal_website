a2bcc4c1a1da6425ef34d246014e87c5f259b10b
ClarkChan1 <clark2chan@gmail.com>
2021-07-02 20:52:12 -0700

    finished the enderman teleport every 10 seconds animation with the flake effects

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,6 @@
+// global vars
+let phase = 0;
+
 function showPleaseStandby(computerScreen) {
   computerScreen.src = "gifs/please-stand-by.gif";
 }
@@ -51,19 +54,18 @@ function lightUpSpeaker(speaker) {}
 // function to make speakers light up and play music on mouseover
 function addListenerSpeakers() {
   let speakers = document.querySelectorAll('.speaker');
-  console.log(speakers);
+  // console.log(speakers);
   for (let a = 0; a < speakers.length; a++) {
     speakers[a].addEventListener('mouseover', playMusic);
     speakers[a].addEventListener('mouseover', lightUpSpeaker);
   }
 }
 
-function moveDiglett(e){
-  console.log("in moveDiglett");
+function moveDiglett(e) {
   this.classList.add('fade-out-diglett');
   let diglett = this;
   // make diglett reappear after 3 seconds
-  setTimeout(function(){
+  setTimeout(function() {
     // move diglett to a random location horizontally
     let min = 0;
     let max = 860;
@@ -72,9 +74,54 @@ function moveDiglett(e){
   }, 3000);
 }
 
-function addListenerDiglett(){
+function addListenerDiglett() {
   let diglett = document.querySelector('.diglett');
   diglett.addEventListener('mouseenter', moveDiglett);
+}
+
+function teleportMinecraft() {
+  let minecraft = document.querySelector('.minecraft');
+  let head = minecraft.querySelector('.minecraft-head');
+  head.style.opacity = (phase == 0) ? 0 : 1;
+  // switch the phase of teleport as well
+  phase = (phase == 0) ? 1 : 0;
+}
+
+function addFlakes() {
+  let minecraft = document.querySelector('.minecraft');
+  let flakeGroup = document.createElement('div');
+  let numFlakes = Math.floor(Math.random() * (5 - 3 + 1) + 3);
+  // make flakes
+  for (let x = 0; x < numFlakes; x++) {
+    let flake = document.createElement('div');
+    flake.style.cssText = "height:8px; width:8px;";
+    // set random positions around the head
+    flake.style.top = Math.floor(Math.random() * (56 - (-8) + 1) + (-8)) + 'px';
+    flake.style.left = Math.floor(Math.random() * (64 - (-8) + 1) + (-8)) + 'px';
+    // create the flake
+    for (let a = 0; a < 4; a += 1) {
+      for (let b = (a % 2 == 0) ? 0 : 2; b < 8; b += 4) {
+        let flakeDot = document.createElement('div');
+        flakeDot.classList.add('flake-dot');
+        flakeDot.style.left = b + 'px';
+        flakeDot.style.top = a * 2 + 'px';
+        flake.appendChild(flakeDot);
+      }
+    }
+    flakeGroup.appendChild(flake);
+  }
+  minecraft.appendChild(flakeGroup);
+  setTimeout(function() {
+    minecraft.removeChild(flakeGroup);
+  }, 3000);
+}
+
+function addAnimationMinecraft() {
+  let minecraft = document.querySelector('.minecraft');
+  setInterval(function() {
+    teleportMinecraft();
+    addFlakes();
+  }, 10000);
 }
 
 // calling all the logic
@@ -82,3 +129,4 @@ addListenerComputer();
 addListenerLamp();
 // addListenerSpeakers();
 addListenerDiglett();
+addAnimationMinecraft();
